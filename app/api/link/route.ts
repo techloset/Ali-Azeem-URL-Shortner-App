6833d79dd59be8c5b35ffcfe664f77c0b922435a
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-05-11 16:04:57 +0500

    prisma generate

--- app/api/link/route.ts
+++ app/api/link/route.ts
@@ -1,53 +1,3 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { PrismaClient } from "@prisma/client";
-// // GET
-// export const GET = async (req: NextRequest) => {
-//   try {
-//     const url = await rismaClient.
-//   } catch (error) {
-
-//   }
-// }
-// // POST
-// export const POST = async (request: NextRequest) => {
-//   const prisma = new PrismaClient();
-
-//   try {
-//     const { longUrl } = await request.json();
-
-//     if (!longUrl) {
-//       return new NextResponse("Missing something", {
-//         status: 400,
-//       });
-//     }
-//     try {
-//       const linkList = await prisma.url.create({
-//         data: {
-//           longUrl,
-//         },
-//       });
-
-//       console.log("Created link list:", linkList);
-//       return new NextResponse(
-//         JSON.stringify({ data: linkList, success: true }),
-//         {
-//           status: 200,
-//         }
-//       );
-//     } catch (error) {
-//       console.error("Error creating link list:", error);
-//       return new NextResponse(JSON.stringify(error), { status: 400 });
-//     }
-//   } catch (error) {
-//     console.error("Error parsing request:", error);
-//     return new NextResponse("Internal Server Error", {
-//       status: 500,
-//     });
-//   }
-// };
-//
-//
-//
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth";
@@ -69,7 +19,6 @@ export const POST = async (req: NextRequest) => {
 
   try {
     const { longUrl } = await req.json();
-    // console.log("Received longUrl:", longUrl);
 
     const shortUrl = Math.random().toString(36).substring(2, 16);
 
@@ -83,7 +32,6 @@ export const POST = async (req: NextRequest) => {
         userEmail: userEmail,
       },
     });
-    // console.log("addUrl =>", addUrl);
 
     return NextResponse.json({
       message: "Url created successfully",
@@ -98,39 +46,6 @@ export const POST = async (req: NextRequest) => {
     return NextResponse.json({ message: "Something went wrong" });
   }
 };
-
-// export const PUT = async (req: NextRequest) => {
-//   try {
-//     const { shortId } = await req.json();
-//     // console.log("Received shortId:", shortId);
-
-//     const url = await prismaClient.url.findUnique({
-//       where: { shortUrl },
-//     });
-
-//     if (!url) {
-//       return NextResponse.json({ message: "URL not found" });
-//     }
-
-//     const updatedUrl = await prismaClient.url.update({
-//       where: { shortUrl },
-//       data: { clickCount: url.clickCount + 1 },
-//     });
-
-// console.log("Updated URL =>", updatedUrl);
-
-//     return NextResponse.json({
-//       message: "URL accessed successfully",
-//       longUrl: updatedUrl.longUrl,
-//       shortId: updatedUrl.shortUrl,
-//       clickCount: updatedUrl.clickCount,
-//       id: updatedUrl.id,
-//     });
-//   } catch (err) {
-//     console.error("Error accessing URL:", err);
-//     return NextResponse.json({ message: "Something went wrong" });
-//   }
-// };
 
 export const DELETE = async (req: NextRequest) => {
   try {
